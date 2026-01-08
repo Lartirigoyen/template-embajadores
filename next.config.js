4a91b06acf8086b8e690b8b9fe214a3f0a507ab7
Dianela Sosa <dsosa@lartirigoyen.com>
2026-01-08 16:56:55 -0300

    instalar selector UI

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ const nextConfig = {
   // Configuración para tRPC
   webpack: (config) => {
     config.externals = [...(config.externals || []), 'pg-native'];
+    if (process.env.NODE_ENV === "development") {
+      config.module.rules.push({
+        test: /\.(jsx|tsx)$/,
+        exclude: /node_modules/,
+        enforce: "pre",
+        use: "@dyad-sh/nextjs-webpack-component-tagger",
+      });
+    }
     return config;
   },
 };
